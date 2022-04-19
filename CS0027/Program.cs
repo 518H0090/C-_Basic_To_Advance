cc1503071c33fca5f5a8a3139b84a93a216ef743
518H0090 <nhoxhieuro5@gmail.com>
2022-04-19 21:16:06 +0700

    add CS0027

---
+++ CS0027/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS0027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Test thử");
+        }
+    }
+}
