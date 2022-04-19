f2ac6a93740cabe64ac79369eb58b5a1cebb70da
518H0090 <nhoxhieuro5@gmail.com>
2022-04-19 23:28:34 +0700

    re-learn File and use git

--- CS0027/Program.cs
+++ CS0027/Program.cs
@@ -7,7 +7,29 @@ namespace CS0027
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Add khác đi");
+            string direc = "ABS";
+            string filename = "ABS.text";
+            if (Directory.Exists(direc))
+            {
+                Console.WriteLine("File đã tồn tại");
+            }
+            else
+            {
+                Console.WriteLine("File được tạo");
+                Directory.CreateDirectory(direc);
+            }
+            var path = Path.Combine(direc, filename);
+            if (File.Exists(path))
+            {
+                File.AppendAllText(path, "Đã tồn tại");
+            }
+            else
+            {
+                File.WriteAllText(path, "Khởi tạo file");
+            }
+
+            Console.WriteLine("Đọc file {0}", File.ReadAllText(path));
+
         }
     }
 }
