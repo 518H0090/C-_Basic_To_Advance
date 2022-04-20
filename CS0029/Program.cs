7af7a1072a9b96d7d8dae0eddf4c2006023cd004
518H0090 <nhoxhieuro5@gmail.com>
2022-04-20 21:12:08 +0700

    Sorted List

---
+++ CS0029/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS0029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new SortedList<string, string>();
+            products.Add("Iphone 6", "Iphone - 6 ");
+            products.Add("Iphone 7", "Iphone - 7 ");
+            products.Add("Iphone 8", "Iphone - 8 ");
+            products.Add("Iphone 9", "Iphone - 9 ");
+            products.Add("Iphone 10", "Iphone - 10 ");
+
+            products["Iphone11"] = "Iphone - 11";
+            products["Iphone12"] = "Iphone - 12";
+
+            foreach (KeyValuePair<string, string> p in products)
+            {
+                Console.WriteLine(p.Key + "  " + p.Value);
+            }
+
+            Console.WriteLine("--------------------");
+
+            foreach (var p in products)
+            {
+                Console.WriteLine(p.Key + "  " + p.Value);
+            }
+
+            Console.WriteLine("--------------------");
+
+            string product10 = "Iphone11";
+            Console.WriteLine($"{product10} : {products[product10]}");
+
+            Console.WriteLine("--------------------");
+
+            foreach (var key in products.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            foreach (var value in products.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("--------------------");
+
+        }
+    }
+}
