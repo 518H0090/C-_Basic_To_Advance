c15b11a7610a0a9bc7129dd2a8d200f88b7d0b10
518H0090 <nhoxhieuro5@gmail.com>
2022-04-22 12:20:22 +0700

    Stack and Queue

---
+++ CS0030/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS0030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Queue
+            Queue<string> queueString = new Queue<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                queueString.Enqueue("haha" + (i + 1));
+            }
+
+            while (queueString.Count > 0)
+            {
+                var getString = queueString.Dequeue();
+                Console.WriteLine($"get {getString} reset of value : {queueString.Count}");
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            // Stack
+            Stack<string> stackString = new Stack<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                stackString.Push("Hello" + (i + 1));
+            }
+
+            for (int i = 0; i < stackString.Count; i++)
+            {
+                var valueString = stackString.Pop();
+                Console.WriteLine($"get {valueString} reset of value : {stackString.Count}");
+            }
+
+        }
+    }
+}
