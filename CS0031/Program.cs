568fb4a23b66eb822876060a7c7a9ca75a3939b8
518H0090 <nhoxhieuro5@gmail.com>
2022-04-22 14:25:32 +0700

    Linkedlist

---
+++ CS0031/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS0031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            // LinkedList
+            LinkedList<string> StringList = new LinkedList<string>();
+
+            StringList.AddFirst("Hello 3");
+            StringList.AddLast("Hello 2");
+            StringList.AddFirst("Hello 1");
+            StringList.AddFirst("Hello 5");
+
+            Console.WriteLine("Từ đầu tới cuối");
+            LinkedListNode<string> nodeFirst = StringList.First;
+
+            while (nodeFirst != null)
+            {
+                Console.WriteLine(nodeFirst.Value);
+                nodeFirst = nodeFirst.Next;
+            }
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Từ cuối tới đầu");
+            LinkedListNode<string> nodeLast = StringList.Last;
+            while (nodeLast != null)
+            {
+                Console.WriteLine(nodeLast.Value);
+                nodeLast = nodeLast.Previous;
+            }
+            Console.WriteLine("--------------------------------------");
+
+        }
+    }
+}
