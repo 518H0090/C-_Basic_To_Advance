2ede3a68fa66414904208fd0e7f6fe6a952211d0
518H0090 <nhoxhieuro5@gmail.com>
2022-05-03 15:03:33 +0700

    rewrite TYpe

---
+++ CS0040/Program.cs
@@ -0,0 +1,43 @@
+﻿// ôn tập type
+using System;
+
+namespace CS0040
+{
+    class User
+    {
+        public string userName { set; get; }
+        public string passWord { set; get; }
+        public long phoneNumber { set; get; }
+
+        public string Address { set; get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User()
+            {
+                userName = "chaymetwa",
+                passWord = "00992",
+                phoneNumber = 252525252,
+                Address = "TPHCM"
+            };
+
+            Type t1 = user.GetType();
+            Console.WriteLine(t1.FullName);
+
+            var propertyNeed = t1.GetProperties();
+
+            foreach (var item in propertyNeed)
+            {
+                var NameProperty = item.Name;
+                Console.WriteLine($"Name Property: {NameProperty}");
+
+                // Lấy dữ liệu property từ user
+                var ValueProperty = item.GetValue(user);
+                Console.WriteLine($"Value Property: {ValueProperty}");
+            }
+        }
+    }
+}
