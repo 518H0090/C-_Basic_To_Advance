78b5ae1da11f212979b0793d4b01f100c3689677
518H0090 <nhoxhieuro5@gmail.com>
2022-05-03 16:05:44 +0700

    Interveted depency

---
+++ CS0043/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace CS0043
+{
+
+    interface IClassB
+    {
+        public void ActionB();
+    }
+
+    interface IClassC
+    {
+        public void ActionC();
+    }
+
+    class ClassC : IClassC
+    {
+        public void ActionC() => Console.WriteLine("Action in ClassC");
+    }
+
+    class ClassB : IClassB
+    {
+        // Phụ thuộc của ClassB là ClassC
+        IClassC c_dependency;
+
+        public ClassB(IClassC classc) => c_dependency = classc;
+        public void ActionB()
+        {
+            Console.WriteLine("Action in ClassB");
+            c_dependency.ActionC();
+        }
+    }
+
+    class ClassA
+    {
+        // Phụ thuộc của ClassA là ClassB
+        IClassB b_dependency;
+
+        public ClassA(IClassB classb) => b_dependency = classb;
+        public void ActionA()
+        {
+            Console.WriteLine("Action in ClassA");
+            b_dependency.ActionB();
+        }
+    }
+
+    class ClassC1 : IClassC
+    {
+        public ClassC1() => Console.WriteLine("ClassC1 is created");
+        public void ActionC()
+        {
+            Console.WriteLine("Action in C1");
+        }
+    }
+
+    class ClassB1 : IClassB
+    {
+        IClassC c_dependency;
+        public ClassB1(IClassC classc)
+        {
+            c_dependency = classc;
+            Console.WriteLine("ClassB1 is created");
+        }
+        public void ActionB()
+        {
+            Console.WriteLine("Action in B1");
+            c_dependency.ActionC();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IClassC objectC = new ClassC1();
+            IClassB objectB = new ClassB1(objectC);
+            ClassA objectA = new ClassA(objectB);
+
+            objectA.ActionA();
+        }
+    }
+}
