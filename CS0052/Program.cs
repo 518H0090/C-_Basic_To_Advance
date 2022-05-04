913a5e8b21b3b13bd81bc85dcf769665ea83738d
518H0090 <nhoxhieuro5@gmail.com>
2022-05-04 20:00:28 +0700

    HttpResponseMessage

---
+++ CS0052/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace CS0052
+{
+    class Program
+    {
+        static async Task<string> GetWebContent(string url)
+        {
+            using var httpClient = new HttpClient();
+            try
+            {
+                httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml+json");
+                var response = await httpClient.GetAsync(url);
+                var htmlValue = await response.Content.ReadAsStringAsync();
+                await ShowHeaders(response.Headers);
+
+                return htmlValue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Loi xay ra la : {e.Message}");
+                return "Loi";
+            }
+        }
+
+        public static async Task ShowHeaders(HttpHeaders headers)
+        {
+            Console.WriteLine("Cac header");
+            foreach (var header in headers)
+            {
+                foreach (var value in header.Value)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            string url1 = "https://xuanthulab.net/networking-su-dung-httpclient-trong-c-tao-cac-truy-van-http.html";
+            await GetWebContent(url1);
+        }
+    }
+}
