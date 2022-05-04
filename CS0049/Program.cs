6fcc9f0cdc82bdfd6823d0eb55785de92d790bce
518H0090 <nhoxhieuro5@gmail.com>
2022-05-04 10:10:08 +0700

    MutipartFormData

--- CS0049/Program.cs
+++ CS0049/Program.cs
@@ -16,12 +16,29 @@ namespace CS0049
             httpMessageRequest.Headers.Add("User-Agent", "Mozilla/5.0");
 
 
-            var parammeters = new List<KeyValuePair<string, string>>();
-            parammeters.Add(new KeyValuePair<string, string>("Key1", "Value1"));
-            parammeters.Add(new KeyValuePair<string, string>("Key2", "Value2"));
-            parammeters.Add(new KeyValuePair<string, string>("Key3", "Value3"));
-            parammeters.Add(new KeyValuePair<string, string>("Key4", "Value4"));
-            var content = new FormUrlEncodedContent(parammeters);
+            // var parammeters = new List<KeyValuePair<string, string>>();
+            // parammeters.Add(new KeyValuePair<string, string>("Key1", "Value1"));
+            // parammeters.Add(new KeyValuePair<string, string>("Key2", "Value2"));
+            // parammeters.Add(new KeyValuePair<string, string>("Key3", "Value3"));
+            // parammeters.Add(new KeyValuePair<string, string>("Key4", "Value4"));
+            // var content = new FormUrlEncodedContent(parammeters);
+
+            // string data = @"
+            //     ""key1"" : ""giatri1"",
+            //     ""key2"" : ""giatri2""
+            // ";
+
+            // var content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+
+            var content = new MultipartFormDataContent();
+            // upload file 1.txt
+            Stream fileStream = File.OpenRead("1.txt");
+            var fileUploads = new StreamContent(fileStream);
+
+            content.Add(fileUploads, "fileupload", "abc.xyz");
+
+            // 
+            content.Add(new StringContent("Value1"), "Key1");
 
             httpMessageRequest.Content = content;
 
@@ -31,6 +48,7 @@ namespace CS0049
             string getString = await httpResponeMessage.Content.ReadAsStringAsync();
 
             Console.WriteLine(getString);
+
         }
     }
 }
